1e32f3a4f81e26df94c8f1f183286e65cd253689
efzr <emerson.zapatarivas@gmail.com>
2024-09-10 21:37:55 -0600

    Learning nvim

--- lua/plugins/csharp.lua
+++ lua/plugins/csharp.lua
@@ -1,12 +1,20 @@
+if true then
+  return {}
+end
+
 return {
   "iabdelkareem/csharp.nvim",
   dependencies = {
-    "williamboman/mason.nvim", -- Required, automatically installs omnisharp
+    "williamboman/mason.nvim",
     "mfussenegger/nvim-dap",
-    "Tastyep/structlog.nvim", -- Optional, but highly recommended for debugging
+    "Tastyep/structlog.nvim",
   },
   config = function()
-    require("mason").setup() -- Mason setup must run before csharp, only if you want to use omnisharp
-    require("csharp").setup()
+    require("mason").setup()
+    require("csharp").setup({
+      dap = {
+        adapter_name = "netcoredbg",
+      },
+    })
   end,
 }
