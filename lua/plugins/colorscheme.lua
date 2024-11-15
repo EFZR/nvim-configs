e8bc4f44b2135264b7ae8e02187e8fa1c118344c
efzr <emerson.zapatarivas@gmail.com>
2024-11-15 06:16:29 +0000

    add some color schems and fix the indentation for each tab

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,10 +1,13 @@
 return {
-	"ellisonleao/gruvbox.nvim", --gruvbox
-	-- "Mofiqul/vscode.nvim",				--vscode
-	-- 'tomasiser/vim-code-dark',		--codedark
+	-- "sainnhe/sonokai", 						--sonokai
+	-- "sainnhe/gruvbox-material", 		--gruvbox-material
+	-- "sainnhe/edge", 								--edge
+	-- "ellisonleao/gruvbox.nvim",		--gruvbox
+	"Mofiqul/vscode.nvim", 						--vscode
+	-- 'tomasiser/vim-code-dark',			--codedark
 	lazy = false,
 	priority = 100,
 	config = function()
-		vim.cmd.colorscheme("gruvbox")
+		vim.cmd.colorscheme("vscode")
 	end,
 }
