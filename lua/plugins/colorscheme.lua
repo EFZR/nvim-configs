07a73845e1b9c3f630f58c09c67faa5b6293e843
efzrbhc <ezapata@banhcafe.hn>
2024-11-20 14:17:55 +0000

    remove the dap keymap

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,10 +1,10 @@
 return {
 	-- "sainnhe/sonokai", 						--sonokai
-	-- "sainnhe/gruvbox-material", 		--gruvbox-material
-	-- "sainnhe/edge", 								--edge
-	"ellisonleao/gruvbox.nvim", --gruvbox
-	-- "Mofiqul/vscode.nvim", 						--vscode
-	-- 'tomasiser/vim-code-dark',			--codedark
+	-- "sainnhe/gruvbox-material",		 		--gruvbox-material
+	-- "sainnhe/edge", 							--edge
+	"ellisonleao/gruvbox.nvim", 				--gruvbox
+	-- "Mofiqul/vscode.nvim", 					--vscode
+	-- 'tomasiser/vim-code-dark',				--codedark
 	lazy = false,
 	priority = 100,
 	config = function()
--- lua/plugins/dapconfig.lua
+++ lua/plugins/dapconfig.lua
@@ -17,23 +17,10 @@ return {
 					return vim.fn.input("path to dll", vim.fn.getcwd() .. "/bin/debug/", "file")
 				end,
 			}
-
-			vim.keymap.set("n", "<F5>", dap.continue, { desc = "Debug: Start/Continue" })
-			vim.keymap.set("n", "<F1>", dap.step_into, { desc = "Debug: Step Into" })
-			vim.keymap.set("n", "<F2>", dap.step_over, { desc = "Debug: Step Over" })
-			vim.keymap.set("n", "<F3>", dap.step_out, { desc = "Debug: Step Out" })
-			vim.keymap.set("n", "<F6>", dap.toggle_breakpoint, { desc = "Debug: Toggle Breakpoint" })
-			vim.keymap.set("n", "<leader>B", function()
-				dap.set_breakpoint(vim.fn.input("Breakpoint condition: "))
-			end, { desc = "Debug: Set Breakpoint" })
 		end,
 	},
 	{
 		"rcarriga/nvim-dap-ui",
 		dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" },
-		config = function()
-			local dapui = require("dapui")
-			vim.keymap.set("n", "<F7>", dapui.toggle, { desc = "Debug: See last session result." })
-		end,
 	},
 }
