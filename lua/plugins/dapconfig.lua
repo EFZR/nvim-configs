3ab3d047b9d8c88ae3383c6e235f88bfd422f96a
efzrbhc <ezapata@banhcafe.hn>
2024-11-21 19:24:35 +0000

    Dapui configs working succesfully

--- lua/plugins/dapconfig.lua
+++ lua/plugins/dapconfig.lua
@@ -28,6 +28,9 @@ return {
 			vim.keymap.set("n", "<F5>", function()
 				dap.continue()
 			end)
+			vim.keymap.set("n", "<F6>", function()
+				dap.restart()
+			end)
 			vim.keymap.set("n", "<F10>", function()
 				dap.step_over()
 			end)
@@ -47,7 +50,7 @@ return {
 				dap.set_breakpoint(nil, nil, vim.fn.input("Log point message: "))
 			end)
 			vim.keymap.set("n", "<Leader>dr", function()
-				dap.repl.open()
+				dap.repl.toggle()
 			end)
 			vim.keymap.set("n", "<Leader>dl", function()
 				dap.run_last()
@@ -71,5 +74,24 @@ return {
 	{
 		"rcarriga/nvim-dap-ui",
 		dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" },
+		config = function()
+			local dap, dapui = require("dap"), require("dapui")
+			dapui.setup()
+			dap.listeners.before.attach.dapui_config = function()
+				dapui.open()
+			end
+			dap.listeners.before.launch.dapui_config = function()
+				dapui.open()
+			end
+			dap.listeners.before.event_terminated.dapui_config = function()
+				dapui.close()
+			end
+			dap.listeners.before.event_exited.dapui_config = function()
+				dapui.close()
+			end
+			vim.keymap.set("n", "<C-n>", function()
+				dapui.toggle()
+			end)
+		end,
 	},
 }
