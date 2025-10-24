7563311edc7111cdc75c76d73158eea7c4f3e586
Emerson Fernando Zapata Rivas <emerson.zapatarivas@gmail.com>
2025-10-24 10:49:21 -0600

    fix: tabs in markdown, autocmds

--- lua/user/autocmds.lua
+++ lua/user/autocmds.lua
@@ -49,3 +49,60 @@ vim.api.nvim_create_autocmd("FileType", {
 	end,
 	desc = "Set indentation for Rust files",
 })
+
+-- Set indentation for TypeScript files
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = "ts",
+	callback = function()
+		vim.opt_local.shiftwidth = 2
+		vim.opt_local.tabstop = 2
+		vim.opt_local.smarttab = true
+	end,
+	desc = "Set indentation for TypeScript files",
+})
+
+-- Set indentation for TypeScript React files
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = "tsx",
+	callback = function()
+		vim.opt_local.shiftwidth = 2
+		vim.opt_local.tabstop = 2
+		vim.opt_local.smarttab = true
+	end,
+	desc = "Set indentation for TypeScript React files",
+})
+
+-- Set indentation for JavaScript files
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = "js",
+	callback = function()
+		vim.opt_local.shiftwidth = 2
+		vim.opt_local.tabstop = 2
+		vim.opt_local.smarttab = true
+	end,
+	desc = "Set indentation for JavaScript files",
+})
+
+-- Set indentation for JavaScript React files
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = "jsx",
+	callback = function()
+		vim.opt_local.shiftwidth = 2
+		vim.opt_local.tabstop = 2
+		vim.opt_local.smarttab = true
+	end,
+	desc = "Set indentation for JavaScript React files",
+})
+
+-- Set indentation for JavaScript React files
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = "markdown",
+	callback = function()
+		vim.opt_local.expandtab = true
+		vim.opt_local.shiftwidth = 3
+		vim.opt_local.tabstop = 3
+		vim.opt_local.softtabstop = 3
+		vim.opt_local.smarttab = true
+	end,
+	desc = "Set indentation to 3 spaces for Markdown files",
+})
